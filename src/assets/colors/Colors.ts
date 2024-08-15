bdce7ce6283f08860cda4e285b4c08f79455c4c6
CuDuyDuc <duccu1403@gmail.com>
2024-08-15 20:14:37 +0700

    push UI

---
+++ src/assets/colors/Colors.ts
@@ -0,0 +1,11 @@
+const COLORS = {
+    BLACK: '#000000',
+    WHITE: '#FFFFFF',
+    HEX_LIGHT_GRAY : '#52555A',
+    HEX_LIGHT_GREY: '#AEAEAE',
+    HEX_LIGHT: '#141921',
+    LIGHT: '#d9e3f0',
+    ORANGE: '#FF9228'
+}
+
+export default COLORS;
---
+++ src/assets/images/index.ts
@@ -0,0 +1,14 @@
+const IMAGES = {
+    Cloud: require('./cloud.png'),
+    EmptyStar: require('./emptyStar.png'),
+    fullStar: require('./fullStar.png'),
+    HeavenRain: require('./heavyrain.png'),
+    Line: require('./line.png'),
+    Mist: require('./mist.png'),
+    ModeRateRain: require('./moderaterain.png'),
+    PartlyCloudy: require('./partlycloudy.png'),
+    Sun: require('./sun.png'),
+    Splash: require('./splash.png')
+}
+
+export default IMAGES
